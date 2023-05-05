0ca0ba88949b629376df075779aaf7749ebeeead
Korntawat Ariyasajjakorn <korntawat.a@gmail.com>
2023-05-05 16:20:16 +0700

    Added images for each show

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const animeList = document.querySelector("#airing-list")
 async function getAiringAnime() {
     let response = await fetch("https://api.jikan.moe/v4/seasons/now")
     let jsonResponse = await response.json()
-    jsonResponse.data.forEach(a => {
-        animeItem = document.createElement("li")
-        animeItem.innerText = a.title_english || a.title
-        animeList.append(animeItem)
-    })
-
+    console.log(jsonResponse)
     const lastPage = jsonResponse.pagination.last_visible_page
+
     for (let i = 1; i <= lastPage; i++) {
         response = await fetch(`https://api.jikan.moe/v4/seasons/now?page=${i}`)
         jsonResponse = await response.json()
         jsonResponse.data.forEach(a => {
             animeItem = document.createElement("li")
+            animeItem.setAttribute("class", "show")
             animeItem.innerText = a.title_english || a.title
+
+            itemImage = document.createElement("img")
+            itemImage.setAttribute("class", "show-image")
+            itemImage.setAttribute("src", a.images.jpg.image_url)
+            animeItem.append(itemImage)
+
             animeList.append(animeItem)
         })
     }
