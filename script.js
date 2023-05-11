d401fa69dc01a13f273ea3aef3025e5ae05c8c38
Korntawat Ariyasajjakorn <korntawat.a@gmail.com>
2023-05-11 11:18:26 +0700

    Adds countdown to airing time for each show
    The airing time currently is based off of when each item is fetched

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const animeList = document.querySelector("#airing-list")
 
+let timeNow = new Date().toLocaleString("en-US", {hour12: false, timeZone: "Asia/Tokyo"})
+let hourNow = timeNow.split(' ')[1].slice(0, 2)
+let minNow = timeNow.split(' ')[1].slice(3, 5)
+let dayNow = new Date().toLocaleString("en-US", {timeZone: "Asia/Tokyo", weekday: "long"})
+// Array for storing numerical value corresponding to a day (i.e. Monday = 0, Tuesday = 1, ...)
+const dayArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+// Whenever user clicks on the refresh button, get current time
+const refreshButton = document.querySelector("#refresh-btn")
+refreshButton.addEventListener("click", () => {
+    timeNow = new Date().toLocaleString("en-US", {hour12: false, timeZone: "Asia/Tokyo"})
+    hourNow = timeNow.split(' ')[1].slice(0, 2)
+    minNow = timeNow.split(' ')[1].slice(3, 5)
+    dayOfWeek = new Date().toLocaleString("en-US", {timeZone: "Asia/Tokyo", weekday: "long"})
+})
+
 async function getAiringAnime() {
     let response = await fetch("https://api.jikan.moe/v4/seasons/now")
     let jsonResponse = await response.json()
@@ -31,7 +47,27 @@ async function getAiringAnime() {
             showTitleContainer.append(showTitleJapanese)
             show.append(showTitleContainer)
 
-
+            // Adds countdown until the show airs
+            const showCountDownContainer = document.createElement("div")
+            showCountDownContainer.setAttribute("class", "show-countdown")
+            const timer = document.createElement("p")
+            let minDiff = a.broadcast.time.split(':')[1] - minNow
+            let hourDiff = a.broadcast.time.split(':')[0] - hourNow
+            let dayDiff = dayArray.indexOf(a.broadcast.day.slice(0, -1)) - dayArray.indexOf(dayNow)
+            if (minDiff < 0) {
+                minDiff += 60
+                hourDiff -= 1
+            }
+            if (hourDiff < 0) {
+                hourDiff += 24
+                dayDiff -= 1
+            }
+            if (dayDiff < 0) {
+                dayDiff += 7
+            }
+            timer.innerText = `${dayDiff}:${hourDiff}:${minDiff}`
+            showCountDownContainer.append(timer)
+            show.append(showCountDownContainer)
 
             animeList.append(show)
         })
